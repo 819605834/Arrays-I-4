8867540ad25ad14f2adfc744f76467367ca09e7a
819605834 <124411012+819605834@users.noreply.github.com>
2026-06-11 01:49:24 +0000

    Modified ranks array

--- main.js
+++ main.js
@@ -2,3 +2,15 @@
 var ranks = ["domain", "kingdom", "category", "family", "genus"];
 
 // Write your code below
+// 1. domain
+// 2. kingdom
+// 3. phylum
+// 4. class
+// 5. order
+// 6. family
+// 7. genus
+// 8. species
+ranks.splice(2, 1, "phylum", "class", "order");
+ranks.splice(7, 0, "species");
+
+console.log(ranks);
